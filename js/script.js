e5be2c5e3b045135f67e332ace597c7c6825493e
zhenli520 <zhenli8866@gmail.com>
2019-10-30 22:32:55 +0000

    test

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
+    let input = $(".input").val();
+    let letter = "a";
+    let vowelsList = ["a", "e", "i", "o", "u"];
+    vowelsList.forEach(function(vowel){
+        if (letter = vowel){
+            input = input + "ay";
+            return input
+        } else(letter != vowel){
+
+        }
+    })
+})
 
 
 
-  
-  
-  
-  
+
+
+
 });
